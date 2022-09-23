e71d4458edad41fbdd651689432345a8d75275e5
DelRoos <delanoroosvelt733@gmail.com>
2022-09-23 16:42:01 +0100

    add tablet view

--- lib/responsive/devices/desktop_scafold.dart
+++ lib/responsive/devices/desktop_scafold.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_web/responsive/constants.dart';
+import 'package:responsive_web/utils/pub_card.dart';
+import 'package:responsive_web/utils/title_card.dart';
 
 class DesktopScafold extends StatefulWidget {
   const DesktopScafold({super.key});
@@ -18,6 +20,35 @@ class _DesktopScafoldState extends State<DesktopScafold> {
       body: Row(
         children: [
           drawer,
+          Expanded(
+            child: Column(
+              children: [
+                AspectRatio(
+                  aspectRatio: 4,
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: GridView.builder(
+                      itemCount: 4,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4,
+                      ),
+                      itemBuilder: (context, index) {
+                        return PubCard();
+                      },
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 5,
+                    itemBuilder: (context, builder) {
+                      return TitleCard();
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
