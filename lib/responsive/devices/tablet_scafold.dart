6195d6f1ed7f273d827d2963a2db90c9f5fabafd
DelRoos <delanoroosvelt733@gmail.com>
2022-09-23 16:38:52 +0100

    add tablet view

--- lib/responsive/devices/tablet_scafold.dart
+++ lib/responsive/devices/tablet_scafold.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_web/responsive/constants.dart';
+import 'package:responsive_web/utils/pub_card.dart';
+import 'package:responsive_web/utils/title_card.dart';
 
 class TabletScafold extends StatefulWidget {
   const TabletScafold({super.key});
@@ -15,6 +17,33 @@ class _TabletScafoldState extends State<TabletScafold> {
       appBar: appBar,
       backgroundColor: defaultBackground,
       drawer: drawer,
+      body: Column(
+        children: [
+          AspectRatio(
+            aspectRatio: 4,
+            child: SizedBox(
+              width: double.infinity,
+              child: GridView.builder(
+                itemCount: 4,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                ),
+                itemBuilder: (context, index) {
+                  return PubCard();
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, builder) {
+                return TitleCard();
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
